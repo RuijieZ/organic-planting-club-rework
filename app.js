98a3c8807c6dda599928805057c64783e49e018b
Ruijie Zhang <jerrydream123@gmail.com>
2016-07-31 22:47:41 -0700

    still doing setup, changed the default template engine to be ejs, which is html friendly. Prefer writing html than jade

--- app.js
+++ app.js
@@ -6,34 +6,32 @@
 var express = require('express'),
   bodyParser = require('body-parser'),
   methodOverride = require('method-override'),
-  errorHandler = require('error-handler'),
+  errorHandler = require('express-error-handler'),
   morgan = require('morgan'),
   routes = require('./routes'),
   api = require('./routes/api'),
   http = require('http'),
+  ejs = require('ejs'),
   path = require('path');
 
 var app = module.exports = express();
-
-
 /**
  * Configuration
  */
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
 app.use(morgan('dev'));
 app.use(bodyParser());
 app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
-
 var env = process.env.NODE_ENV || 'development';
 
 // development only
 if (env === 'development') {
-  app.use(express.errorHandler());
+  app.use(errorHandler());
 }
 
 // production only
@@ -46,9 +44,8 @@ if (env === 'production') {
  * Routes
  */
 
-// serve index and view partials
+// serve index
 app.get('/', routes.index);
-app.get('/partials/:name', routes.partials);
 
 // JSON API
 app.get('/api/name', api.name);
